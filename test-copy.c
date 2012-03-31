d90ba1b6d7b7f70b291a269081e747eeee1d4622
Rob Clark <robdclark@gmail.com>
2012-03-31 01:20:44 +0000

    put email addr in copyright notices

--- test-copy.c
+++ test-copy.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2012 Rob Clark
+ * Copyright © 2012 Rob Clark <robclark@freedesktop.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
